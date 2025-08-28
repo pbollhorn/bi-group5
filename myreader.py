150e33e17ab1b2a8e66ebba869302b0953b19346
Peter Bollhorn <pbollhorn@gmail.com>
2025-08-28 21:12:20 +0200

    better python naming

--- myreader.py
+++ myreader.py
@@ -12,15 +12,15 @@ def readExcelWorksheetIntoDataFrame(filename):
     return df
 
 
-def readF1RaceResult(race_url):
+def read_f1_race_result(race_url):
     response = requests.get(race_url)
     soup = BeautifulSoup(response.content, "html.parser")
     table = soup.find("table") # Find the table
     string = StringIO(str(table)) # Convert table to string
-    df = pd.read_html(string)[0] # Convert string to pandas
+    df = pd.read_html(string)[0] # Convert string to pandas dataframe
     return df
 
-def readF1SeasonResults(year):
+def read_f1_season_results(year):
     season_url = "https://www.formula1.com/en/results/" + str(year) + "/races"
     response = requests.get(season_url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -34,7 +34,7 @@ def readF1SeasonResults(year):
         relativeUrl=a['href']
         race_url=season_url+relativeUrl[28:]
         print(race_url)
-        df = readF1RaceResult(race_url) # pandas frame for race result
+        df = read_f1_race_result(race_url) # pandas frame for race result
         print(df)
         
         race_no+=1
