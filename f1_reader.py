03ea89d03909151cb3462e7ee8ecdcef45b40ed1
Peter Bollhorn <pbollhorn@gmail.com>
2025-08-29 20:02:55 +0200

    deleted file

--- f1_reader.py
+++ f1_reader.py
@@ -73,22 +73,22 @@ def read_f1_season_to_dictionary(year, directory):
 
 
 # Read the result for a single F1 race from formula1.com to html file
-def read_f1_race_to_html_file(race_url, file_path):
+def read_f1_race_from_website_to_html_file(race_url, file_path):
     response = requests.get(race_url)
     with open(file_path, "wb") as f:
             f.write(response.content)
 
 # Read the results of all F1 races of a season from formula1.com to html files
-def read_f1_season_to_html_files(year, directory):
-    season_url = "https://www.formula1.com/en/results/" + str(year) + "/races"
+def read_f1_season_from_website_to_html_files(year, directory):
+    season_url = f"https://www.formula1.com/en/results/{year}/races"
     response = requests.get(season_url)
     soup = BeautifulSoup(response.content, "html.parser")
-    table = soup.find("table") # Find the first table element in the html, because that contains the season overview
+    table = soup.find("table") # Find the first table element in the html, because that contains the season race overview
     race_no = 0
     for a in table.find_all("a"): # Find all the a elements in the table, because they are the links to the races
         race_no += 1
-        url = a['href']
-        race_url = season_url + url[28:]
-        file_path = directory + str(year) + "-" + str(race_no) + ".html"
-        read_f1_race_to_html_file(race_url, file_path)
+        href = a['href']
+        race_url = season_url + href[28:]
+        file_path = os.path.join(directory, f"{year}-{race_no}.html")
+        read_f1_race_from_website_to_html_file(race_url, file_path)
         
