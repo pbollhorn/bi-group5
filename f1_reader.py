fa9074b0d61bbb2dabe9dc1d3e297b6972a7f1df
Peter Bollhorn <pbollhorn@gmail.com>
2025-08-29 22:18:18 +0200

    now removes Note from data frame

--- f1_reader.py
+++ f1_reader.py
@@ -38,45 +38,47 @@ def read_f1_season_results(year):
         
     return season_results
 
-# Helper function to fix driver name: Replace escape code with space, and add space before 3-letter code
-def fix_driver_name(original):
-    fixed = original.replace("\xa0", " ")
-    fixed = fixed[0:-3] + " " + fixed[-3:]
-    return fixed
+
 
 # Read the result for a single F1 race from html file to data frame
-def read_f1_race_to_data_frame(file_path):
-    # file_path="html/1950-1.html"
+def read_f1_race_from_html_file_to_data_frame(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         html_content = f.read()
     soup = BeautifulSoup(html_content, "html.parser")
     table = soup.find("table") # Find the first table element in the html, because that contains the race result
     df = pd.read_html(StringIO(str(table)))[0] # Convert table to pandas data frame
+    
     df["DRIVER"] = df["DRIVER"].apply(fix_driver_name)
+    
+    # Check if last row in "POS." starts with "Note "
+    if df["POS."].iloc[-1].startswith("Note "):
+        df = df.iloc[:-1]  # Drop the last row
+    
     return df
 
+
 # Read the results of all F1 races of a season from html files to a dictionary of data frames
-def read_f1_season_to_dictionary(year, directory):
+def read_f1_season_from_html_files_to_dictionary(year, directory):
     
     race_no = 0
     season = {}
     
     while True:
         race_no += 1
-        file_path = directory + str(year) + "-" + str(race_no) + ".html"
+        file_path = os.path.join(directory, f"{year}-{race_no}.html")
         if not os.path.exists(file_path):
             break
-        season[race_no] = read_f1_race_to_data_frame(file_path)
+        season[race_no] = read_f1_race_from_html_file_to_data_frame(file_path)
     
     return season
     
-
-
+    
 # Read the result for a single F1 race from formula1.com to html file
 def read_f1_race_from_website_to_html_file(race_url, file_path):
     response = requests.get(race_url)
     with open(file_path, "wb") as f:
             f.write(response.content)
+
 
 # Read the results of all F1 races of a season from formula1.com to html files
 def read_f1_season_from_website_to_html_files(year, directory):
@@ -92,3 +94,9 @@ def read_f1_season_from_website_to_html_files(year, directory):
         file_path = os.path.join(directory, f"{year}-{race_no}.html")
         read_f1_race_from_website_to_html_file(race_url, file_path)
         
+
+# Helper function to fix driver name: Replace escape code with space, and add space before 3-letter code
+def fix_driver_name(original):
+    fixed = original.replace("\xa0", " ")
+    fixed = fixed[0:-3] + " " + fixed[-3:]
+    return fixed
