f1f41f741ebcc33787c1c7cc1570f502f17dbdbd
Peter Bollhorn <pbollhorn@gmail.com>
2025-09-04 11:41:32 +0200

    progression

--- reader/generic_reader.py
+++ reader/generic_reader.py
@@ -4,6 +4,6 @@ def read_csv_file_to_data_frame(file_path):
     df = pd.read_csv(file_path)
     return df
 
-def read_excel_file_to_data_frame(file_path):
-    df = pd.read_excel(file_path)
+def read_excel_file_to_data_frame(file_path, skiprows=None):
+    df = pd.read_excel(file_path, skiprows=skiprows)
     return df
---
+++ test.py
@@ -0,0 +1,2 @@
+from reader import f1_reader
+from reader import generic_reader
